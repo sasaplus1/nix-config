aa2d1a9d93e6173703f57a5bb9a2a79543232905
sasaplus1 <sasaplus1@gmail.com>
2026-09-11 10:55:08 +0900

    feat: set the fcitx5 group layout to jp
    fcitx5 creates its default group with Layout=us and translates keycodes
    with that keymap. Under us, keycode 92 and 94 carry no keysym, so the
    Henkan and Muhenkan hotkeys never fired.

--- modules/japanese.nix
+++ modules/japanese.nix
@@ -19,6 +19,28 @@
       fcitx5-mozc
     ];
 
+    # 既定のグループは Layout=us で作られる
+    # us では keycode 92 と 94 にキーシムが割り当たらず下の Hotkey が死ぬ
+    fcitx5.settings.inputMethod = {
+      "Groups/0" = {
+        Name = "Default";
+        "Default Layout" = "jp";
+        DefaultIM = "mozc";
+      };
+
+      "Groups/0/Items/0" = {
+        Name = "keyboard-jp";
+        Layout = "";
+      };
+
+      "Groups/0/Items/1" = {
+        Name = "mozc";
+        Layout = "";
+      };
+
+      GroupOrder."0" = "Default";
+    };
+
     # かなキーでオン、英数キーでオフにする
     # UTM は かな を keycode 92、英数 を 94 で渡してくるので
     # jp レイアウトではそれぞれ Henkan と Muhenkan になる
